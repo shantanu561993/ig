d33be44267944c3ce4c295d75431ddcd04878041
nixawk <hap.ddup@gmail.com>
2016-11-17 07:08:39 -0600

    add domain webspider

---
+++ ig/webspider_domain.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from bing import bing
+from baidu import baidu
+from yahoo import yahoo
+import re
+
+
+class baidu_domain_spider(baidu):
+    def __init__(self):
+        super(baidu_domain_spider, self).__init__()
+
+    def baidu_domain_search(self, domain, page=2, random_sleep=True):
+        """parse domains from baidu spider results"""
+        dork = "site:{}".format(domain)
+        regex = re.compile('[a-zA-Z0-9]+\.{}'.format(domain), re.I | re.M)
+        results = self.baidu_dork_search(dork, page=page,
+                                         random_sleep=random_sleep)
+        data = results[dork]  # [['title', 'href', 'link'], ... ]
+        bd_domains = []
+
+        if not data:
+            return {domain: {'baidu': bd_domains}}
+
+        for title, href, link in data:
+            domains = regex.findall(href)
+            for _ in domains:
+                if _ in bd_domains:
+                    continue
+                bd_domains.append(_)
+
+        return {domain: {'baidu': bd_domains}}
+
+
+class bing_domain_spider(bing):
+    def __init__(self):
+        super(bing_domain_spider, self).__init__()
+
+    def bing_domain_search(self, domain, page=2, random_sleep=False):
+        """parse domains from bing spider results"""
+        dork = "site:{}".format(domain)
+        regex = re.compile('[a-zA-Z0-9]+\.{}'.format(domain), re.I | re.M)
+        results = self.bing_dork_search(dork, page=page,
+                                        random_sleep=random_sleep)
+        data = results[dork]  # [['title', 'href']]
+        bi_domains = []
+
+        if not data:
+            return {domain: {'bing': bi_domains}}
+
+        for title, href in data:
+            domains = regex.findall(href)
+            for _ in domains:
+                if _ in bi_domains:
+                    continue
+                bi_domains.append(_)
+
+        return {domain: {'bing': bi_domains}}
+
+
+class yahoo_domain_spider(yahoo):
+    def __init__(self):
+        super(yahoo_domain_spider, self).__init__()
+
+    def yahoo_domain_search(self, domain, page=2, random_sleep=True):
+        """parse domains from yahoo spider results"""
+        dork = "site:{}".format(domain)
+        regex = re.compile('[a-zA-Z0-9]+\.{}'.format(domain), re.I | re.M)
+        results = self.yahoo_dork_search(dork, page=page,
+                                         random_sleep=random_sleep)
+        data = results[dork]  # [['title', 'href'], ... ]
+        yh_domains = []
+
+        if not data:
+            return {domain: {'yahoo': yh_domains}}
+
+        for title, href in data:
+            domains = regex.findall(href)
+            for _ in domains:
+                if _ in yh_domains:
+                    continue
+                yh_domains.append(_)
+
+        return {domain: {'yahoo': yh_domains}}
+
+
+class domainspider(baidu_domain_spider,
+                   bing_domain_spider,
+                   yahoo_domain_spider):
+    def __init__(self):
+        super(domainspider, self).__init__()
+
+    def search(self, domain, page=2, random_sleep=True):
+        domains = []
+        bdret = self.baidu_domain_search(domain, page=page,
+                                         random_sleep=random_sleep)
+        biret = self.bing_domain_search(domain, page=page,
+                                        random_sleep=random_sleep)
+        yhret = self.yahoo_domain_search(domain, page=page,
+                                         random_sleep=random_sleep)
+
+        domains.extend(bdret[domain]['baidu'])
+        domains.extend(biret[domain]['bing'])
+        domains.extend(yhret[domain]['yahoo'])
+
+        return list(set(domains))
+
+
+if __name__ == '__main__':
+    domain = 'google.com'
+    ds = domainspider()
+    print(ds.search(domain, page=1, random_sleep=True))
