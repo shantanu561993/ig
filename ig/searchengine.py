fb0f1fa50aa07ea96f63ecae4782504e8aa90562
nixawk <hap.ddup@gmail.com>
2016-11-17 07:10:26 -0600

    commit web spider functions

--- ig/searchengine.py
+++ ig/searchengine.py
@@ -6,11 +6,11 @@ class searchengine(object):
     def __init__(self):
         super(searchengine, self).__init__()
 
-    def baidu_dork_search(self):
-        pass
+    # def baidu_dork_search(self):
+    #     pass
 
-    def bing_dork_search(self):
-        pass
+    # def bing_dork_search(self):
+    #     pass
 
-    def yahoo_dork_search(self):
-        pass
+    # def yahoo_dork_search(self):
+    #     pass
